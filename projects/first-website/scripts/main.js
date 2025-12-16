db3b72ff415c183dae84865f6102e860073e7a9b
Ezekiel Bassey <bazzdude007@gmail.com>
2025-12-16 21:12:40 +0100

    added main.js, updated web-dev.html and updated stylesheet

---
+++ projects/first-website/scripts/main.js
@@ -0,0 +1,25 @@
+const headerTop = document.querySelector("#header-top");
+const headerMain = document.querySelector("#header-main");
+const mainHeader = document.querySelector("h1");
+mainHeading.style.color = window.getComputedStyle(document.querySelector("#header-main"))["backgroundColor"];
+
+function switchColor() {
+    let sibling;
+    if (this.nextElementSibling)
+        sibling = this.nextElementSibling;
+    else
+        sibling = this.previousElementSibling;
+
+    if (this.classList.contains("black")) {
+        this.classList.replace("black", "burlywood");
+        sibling.classList.replace("burlywood", "black");
+        mainHeading.style.color = window.getComputedStyle(document.querySelector("#header-main"))["backgroundColor"];
+    } else {
+        this.classList.replace("burlywood", "black");
+        sibling.classList.replace("black", "burlywood");
+        mainHeading.style.color = window.getComputedStyle(document.querySelector("#header-main"))["backgroundColor"];
+    }
+}
+
+headerTop.addEventListener("click", switchColor);
+headerMain.addEventListener("click", switchColor);
